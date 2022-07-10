0831c2c36c11f2b91099a3a939568fe8bb1e2ad7
Camryn Poole <camrynannpoole@gmail.com>
2022-07-09 20:31:37 -0400

    cpp

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -9,6 +9,7 @@ local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
 
 -- https://github.com/prettier-solidity/prettier-plugin-solidity
+
 null_ls.setup {
   debug = false,
   sources = {
@@ -18,7 +19,6 @@ null_ls.setup {
     },
     formatting.black.with { extra_args = { "--fast" } },
     formatting.stylua,
-    formatting.google_java_format,
     diagnostics.flake8,
   },
 }
